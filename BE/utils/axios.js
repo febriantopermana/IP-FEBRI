0f8409736e1082608eec50f16840ef41426568e6
febriantopermana <febriantopermana16@gmail.com>
2024-08-08 22:55:04 +0700

    revition .

--- BE/utils/axios.js
+++ BE/utils/axios.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
 const instance = axios.create({
-    baseURL: 'https://api.jikan.moe/v4/'
+    baseURL: 'https://api.jikan.moe/v4/',
+    timeout: 10000
   });
 
 module.exports = { instance }
